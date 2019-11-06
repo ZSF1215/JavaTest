d011aef348d3428bfa8110ba71cf20b80311116b
ZSF1215 <fan121535@163.com>
2019-11-06 22:30:08 +0800

    无头双向

---
+++ DoubleList.java
@@ -0,0 +1,48 @@
+class ListNode {
+    public int data;
+    public ListNode prev;//前驱
+    public ListNode next;//后继
+    //new ListNode(10);
+    public ListNode(int data) {
+        this.data = data;
+    }
+}
+
+class DoubleList {
+    public ListNode head;//头
+    public ListNode last;//尾巴
+    //头插法
+    public void addFirst(int data) {
+        ListNode node = new ListNode(data);
+        if(this.head == null) {
+            this.head = node;
+            this.last = node;
+        }else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+    }
+    public void addLast(int data) {
+        ListNode node = new ListNode(data);
+        if(this.head == null) {
+            this.head = node;
+            this.last = node;
+        }else {
+            this.last.next = node;
+            node.prev = this.last;
+            this.last = node;
+        }
+    }
+    public void display() {
+        if(this.head == null) {
+            return;
+        }
+        ListNode cur = this.head;
+        while (cur != null) {
+            System.out.print(cur.data+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+}
